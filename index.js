1600ef5f770822662f0ccb8c0d7b302429fdb323
Tazwer Shakil <tazwershakilshakil@gmail.com>
2025-10-19 17:10:45 +0600

    added apply data post and get system and added fing single job data with id

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const app = express()
 require('dotenv').config();
-const { MongoClient } = require('mongodb');
+const { MongoClient, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
 // middleware
 app.use(cors())
@@ -10,14 +10,41 @@ app.use(express.json())
 // mongodb connection--
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.gr8kgxz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri)
 
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
-    // Send a ping to confirm a successful connection
+    // jobs data collection in database 
+    const jobsCollection = client.db("careerLink").collection("jobs")
+    const applyCollection = client.db("careerLink").collection("applicant")
+    // jobs api
+    app.get('/jobs', async (req, res)=>{
+        const cursor = jobsCollection.find();
+        const result = await cursor.toArray()
+        res.send(result)
+    })
+    // get job single data 
+    app.get(`/jobs/:id`, async (req, res)=>{
+        const id = req.params.id
+        const query = {_id : new ObjectId(id)}
+        const result = await jobsCollection.findOne(query)
+        res.send(result)
+    })
+    // applicant data post 
+    app.post('/application', async (req, res)=>{
+      const application = req.body;
+      const result = await applyCollection.insertOne(application)
+      res.send(result)
+    })
+    // get apply data
+    app.get('/application', async (req, res)=>{
+      // const application = req.body;
+      const query = applyCollection.find()
+      const result = await query.toArray()
+      res.send(result)
+    })
+
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
